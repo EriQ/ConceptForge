8648fb62a0ebd02926fc4a6e8a75fb1d91e8fc3a
Eric Higdon <mrbassman.10@gmail.com>
2016-03-20 18:39:39 -0500

    Only load insta feed if online; added ability to cache last saved theme

--- www/js/pageLoad.js
+++ www/js/pageLoad.js
@@ -1,4 +1,6 @@
 var categories = "",
+    url = 'http://widget.websta.me/rss/tag/belugawhale',
+    storage = window.localStorage,
     lists = {
     "dessert": ["almond cookie", "ambrosia", "angelfood cake", "apple brown Betty", "apple crisp", "apple pie", "baked Alaska", "baked apple", "baklava", "banana split", "Belgian waffle", "biscotti", "black forest cake", "blueberry muffin", "bombe", "Boston cream pie", "bread pudding", "brownie", "buttercream frosting", "butterscotch", "cake", "cannoli", "caramel apple", "carrot cake", "cheesecake", "cherry pie", "chocolate bar", "chocolate cake", "chocolate chip cookie", "chocolate mousse", "churro", "cinnamon roll", "cobbler", "coconut cake", "coconut cream pie", "coffee cake", "cookie", "crepe Suzette", "cupcake", "custard", "Danish pastry", "dessert", "devil's food cake", "doughnut", "dumplings", "eclair", "flan", "fortune cookie", "French toast", "fritter", "frosting", "frozen yogurt", "fruit cake", "fruit cocktail", "fruit salad", "fudge", "gelatin", "gelato", "gingersnaps", "gingerbread", "honey", "hot-fudge sundae", "ice cream", "ice cream cake", "icing", "jam", "jelly", "jellyroll", "Key lime pie", "ladyfingers", "lemon bars", "lemon meringue pie", "macaroon", "marshmallow", "meringue", "milkshake", "molasses", "mousse", "muffin", "neapolitan ice cream", "nougat", "nut brittle", "oatmeal cookie", "pancakes", "panna cotta", "parfait", "pastry", "peanut brittle", "peanutbutter cookie", "pecan pie", "pie", "poached pears", "popcicle", "popover", "pound cake", "praline", "pudding", "pumpkin pie", "quick bread", "red velvet cake", "rhubarb pie", "raisin bread", "rice pudding", "scone", "sherbet", "shortbread", "s'mores", "snickerdoodle", "soda", "soda bread", "sorbet", "souffle", "sponge cake", "spumoni", "strawberry shortcake", "strudel", "sugar", "sugar cookie", "sundae", "sweet potato pie", "sweet roll", "sweets", "tapioca pudding", "tart", "toasted marshmallow", "toffee", "torte", "trifle", "truffle", "turnover", "upside-down cake", "vanilla cream pie", "vanilla pudding", "waffle", "watermelon ice", "yellow cake", "zabiglone"],
 
@@ -6,7 +8,28 @@ var categories = "",
 
     "energy": ["absorb", "AC", "accumulator", "alternating current", "anthracite coal", "appliance", "battery", "biodiesel", "biofuel", "biomass", "bituminous coal", "blackout", "boiler", "British thermal unit", "Btu", "capacity", "carbon", "carbon footprint", "carbon tax", "charcoal", "chemical energy", "clean energy", "climate change", "coal", "coke", "combustion", "conservation", "crude oil", "current", "dam", "DC", "diesel", "direct current", "drill", "dynamo", "efficiency", "efficient", "electric", "electrical", "electrical grid", "electromagnetic energy", "electron", "energy", "engine", "engineer", "entropy", "environment", "erg", "ethanol", "fossil fuel", "flexible fuel", "flywheel", "fuel", "fuel cell", "furnace", "gas", "gasoline", "gas-turbine", "generate", "generation", "generator", "geothermal", "global warming", "green", "green energy", "greenhouse effect", "greenhouse gas", "grid", "heat", "heat exchange", "high-voltage", "horsepower", "human-powered", "hybrid", "hydrocarbon", "hydroelectric", "hydrogen", "hydrothermal", "industry", "internal combustion engine", "inverter", "jet fuel", "joule", "Kelvin scale", "kilowatt", "kilowatt-hour", "kinetic energy", "light", "liquefied petroleum gas", "magnetic energy", "megawatt", "methane", "methanol", "mining", "motor", "natural gas", "nuclear", "nuclear energy", "nuclear power", "nuclear reactor", "nucleus", "off-the-grid", "oil", "oil rig", "peak oil", "peat", "petroleum", "photon", "photovoltaic", "photovoltaic panel", "pollution", "potential energy", "power", "power grid", "power lines", "power plant", "power station", "power transmission", "propane", "public utility", "radiant", "radiate", "reactor", "reciprocating engine", "reflect", "renewable", "reservoir", "shale", "solar panel", "solar power", "static electricity", "steam", "steam engine", "steam turbine", "sun", "sunlight", "sunshine", "sustainable", "temperature", "therm", "thermal energy", "thermodynamics", "tidal power", "transmission lines", "transmit", "turbine", "utilities", "volt", "waste", "watt", "wattage", "wave power", "wind", "wind farm", "windmill", "wind power", "wind turbine", "work"]
 };
-    
+function loadInsta(feed) {
+    $.each(feed.responseData.feed.entries, function(index) {
+        $(".hiddenInstagramFeed").append(this.content);
+        if(index == 29)
+        {
+            return false;
+        }
+    });
+    $(".hiddenInstagramFeed").find("img").each(function(){
+        if($(this).height() == "306")
+        {
+            $(".instagramFeed").append('<div class="img" data-comment="'+$(this).parent().parent().next().find("strong").html()+'" data-url="'+$(this).attr("src")+'" style="background: url('+$(this).attr("src")+') no-repeat; background-size:cover; background-position: center center; width: 50%; height: '+$(window).width() / 2+'px"></div>');
+        }
+    });
+    $(".instagramFeed").find(".img").on("click", function(){
+        $("body").append("<div class='backdrop'><div class='fullheight'></div><div class='imageContainer'><img src='"+$(this).attr("data-url")+"'/><i class='fa fa-times'></i><p>"+$(this).attr("data-comment")+"</p></div></div>");
+        $(".backdrop").on("click", function() {
+            $(this).remove();
+        });
+    });
+    $(".hiddenInstagramFeed").empty();
+}    
 
 $(function() {
     $( "[data-role='navbar']" ).navbar();
@@ -72,38 +95,27 @@ $(document).ready(function(){
             $(this).remove(); 
         }).nodoubletapzoom();
     }); 
-    var url = 'http://widget.websta.me/rss/tag/belugawhale',
-        storage = window.localStorage;
-    $.ajax({
-        url:'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
-        cache: false,
-        dataType: "jsonp",
-        success:function(feed) {
-            console.log(feed);
-            $.each(feed.responseData.feed.entries, function(index) {
-                $(".hiddenInstagramFeed").append(this.content);
-                if(index == 29)
-                {
-                    return false;
-                }
-            });
-            $(".hiddenInstagramFeed").find("img").each(function(){
-                if($(this).height() == "306")
-                {
-                    $(".instagramFeed").append('<div class="img" data-comment="'+$(this).parent().parent().next().find("strong").html()+'" data-url="'+$(this).attr("src")+'" style="background: url('+$(this).attr("src")+') no-repeat; background-size:cover; background-position: center center; width: 50%; height: '+$(window).width() / 2+'px"></div>');
-                }
-            });
-            $(".instagramFeed").find(".img").on("click", function(){
-                $("body").append("<div class='backdrop'><div class='fullheight'></div><div class='imageContainer'><img src='"+$(this).attr("data-url")+"'/><i class='fa fa-times'></i><p>"+$(this).attr("data-comment")+"</p></div></div>");
-                $(".backdrop").on("click", function() {
-                    $(this).remove();
-                });
-            });
-            //$(".hiddenInstagramFeed").empty();
-        },
-        fail: function(error){
-            console.log(error);
-        }
-    });
     
+    if(navigator.onLine)
+    {
+        $.ajax({
+            url:'http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=1000&callback=?&q=' + encodeURIComponent(url),
+            cache: false,
+            dataType: "jsonp",
+            success:function(feed) {
+                loadInsta(feed);
+                storage.setItem("instaFeed", JSON.stringify(feed));
+            },
+            fail: function(error){
+                console.log("There was an error :"+error);
+                if(storage.getItem("instaFeed") != null)
+                    loadInsta(JSON.parse(storage.getItem("instaFeed")));
+            }
+        }); 
+    }
+    else
+    {
+        $(".instagramFeed").append("<p>You must be online to view the gallery.</p>")
+    }
+
 });
