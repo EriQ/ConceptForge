c12996d37283ebd2b4b07d0ae332badd391991e9
Eric Higdon <mrbassman.10@gmail.com>
2016-04-04 17:25:04 -0500

    Keep the randomizer container scrolled all the way right

--- www/js/pageLoad.js
+++ www/js/pageLoad.js
@@ -143,7 +143,7 @@ $(document).ready(function(){
     $(".categoriesSmallBar").width(1/$(".categorySlide").length * 100 + "%");
     $(".category").nodoubletapzoom();
     $(".categories .category").on("dblclick", function(e) {
-        $(this).addClass("adding").clone().appendTo(".randomizer").on("dblclick", function() {
+        $(this).clone().appendTo(".randomizer").on("dblclick", function() {
             $(this).find("i").animate(
             {
                 "padding": "0px", 
@@ -169,7 +169,9 @@ $(document).ready(function(){
             "width": "35px",
             "height": "31px"
         }, "fast").parent().nodoubletapzoom();
-        $(this).removeClass("adding")
+        $(".randomizer").animate({
+            "scrollLeft": $(".randomizer").find(".category").width() * $(".randomizer").find(".category").length
+        });
         toggleEnabled();
     }); 
     $( ".categories" ).on( "swipeleft", function(){
